eb73b358f5b16f70fc4737890a7095c970c7e940
Michael <mikeks81@gmail.com>
2016-11-22 23:45:34 -0500

    setting up authentiation with devise

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,12 @@
+class HomeController < ApplicationController
+	before_action :authenticate_user!, except: [:index]
+  def index
+  end
+
+  private
+  helper_method :user
+
+  def user
+  	@user ||= User.where(id: params[:id]).first || User.new
+  end
+end
