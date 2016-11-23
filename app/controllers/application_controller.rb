ced00c2a7368dbdb8ad7e6de9ace1b7db65e34b4
Michael <mikeks81@gmail.com>
2016-11-23 02:22:48 -0500

    authenticate and some styling done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-end
+
+  def after_sign_in_path_for(resource)
+  	user_path(resource)
+  end
+
+  # def after_sign_out_path_for(resource)
+  # 	root_path
+  # end
+end	
--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,4 @@
-class HomeController < ApplicationController
+class HomesController < ApplicationController
 	before_action :authenticate_user!, except: [:index]
   def index
   end
