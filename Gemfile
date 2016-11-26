d3a6ec9089081362579d2fe8d8d38be5d17b88bb
Michael <mikeks81@gmail.com>
2016-11-26 00:46:20 -0500

    added spring, guard config, model: user, contact. factories: user, contact,phones

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ group :development, :test do
   gem 'rspec-rails'
   gem 'factory_girl_rails'
   gem 'guard-rspec'
+  gem 'spring-commands-rspec'
 end
 
 group :test do 
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe "User Validation" do 
+  	let(:user) { create(:user)}
+  	let(:invalid_user) { build(:user, email: "")}
+  	let(:invalid_user_password) { build(:user, password: "")}
+  	let(:invalid_user_password_conf) { build(:user, password_confirmation: "")} 
+
+  	it 'is valid with email' do 
+  		expect(user).to be_valid
+  	end
+
+  	it 'is invalid without email and have error message' do
+  		expect(invalid_user).not_to be_valid
+  		expect(invalid_user.errors[:email]).to include("can't be blank")
+  	end
+
+  	it 'is invalid without password and have error message' do
+  		expect(invalid_user_password).not_to be_valid
+  		expect(invalid_user_password.errors[:password]).to include("can't be blank")
+  	end
+
+  	it 'is invalid without password_confirmation and have error message' do
+  		expect(invalid_user_password_conf).not_to be_valid
+  		expect(invalid_user_password_conf.errors[:password_confirmation]).to include("doesn't match Password")
+  	end
+
+  	it 'is invalid if duplicate email' do
+  		create(:user, email: "this@self.com")
+  		user = build(:user, email: "this@self.com")
+  		user.valid?
+  		expect(user.errors[:email]).to include("has already been taken")
+  	end
+  end
+
+  describe "User Associations" do 
+  	let(:user) { create(:user, :with_contacts, :with_phones)}
+
+  	it 'has many contacts' do 
+  		expect(user.contacts.count).to eq(1)
+  	end
+  	it 'has many phones' do 
+  		expect(user.phones.count).to eq(1)
+  	end
+  end
+end
